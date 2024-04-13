3ed5746263ea8706e9a33bbca87a9bd4586aa82a
dastgir1 <ghulamdustgirsyyed007@gmail.com>
2024-04-13 07:14:25 +0500

    add link brand in side menu

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,11 +1,11 @@
 <aside class="col-md-2  bg-danger text-white "  id="sidemenu">
         <a href="#" class="closebtn ms-auto" onclick="closeSidebar()" style="font-size: 35px;  margin-top: 0px; margin-right: 10px; text-decoration: none; color: #fff; display: block; float: right; overflow: hidden;">&times;</a>
         <ul class="navbar-nav">
-            <li class="nav-item mx-3"><a class="nav-link" href=""><strong>Dash Board</strong></a></li>
+            <li class="nav-item mx-3"><a class="nav-link" href="index.php"><strong>Dash Board</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href=""><strong>Produts</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href="categories.php"><strong>Categories</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href="sub_categories.php"><strong>Sub-Categories</strong></a></li>
-            <li class="nav-item mx-3"><a class="nav-link" href=""><strong>Brands</strong></a></li>
+            <li class="nav-item mx-3"><a class="nav-link" href="brand.php"><strong>Brands</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href=""><strong>Orders</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href=""><strong>Users</strong></a></li>
             <li class="nav-item mx-3"><a class="nav-link" href=""><strong>option</strong></a></li>
